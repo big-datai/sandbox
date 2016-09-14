d8484911d08e6edfd0a7ef51a397dc711d4171b5
Dmitry Pavlov <dpavlov@dmitrys-mbp.corp.cyren.com>
2016-09-14 13:54:16 +0300

    added test classes

---
+++ src/test/scala/MatchTest.scala
@@ -0,0 +1,23 @@
+/**
+  * Created by dpavlov on 13/09/2016.
+  */
+import collection.mutable.Stack
+import org.scalatest._
+
+class MatchTest extends FlatSpec with Matchers {
+
+  "A Stack" should "pop values in last-in-first-out order" in {
+    val stack = new Stack[Int]
+    stack.push(1)
+    stack.push(2)
+    stack.pop() should be (2)
+    stack.pop() should be (1)
+  }
+
+  it should "throw NoSuchElementException if an empty stack is popped" in {
+    val emptyStack = new Stack[Int]
+    a [NoSuchElementException] should be thrownBy {
+      emptyStack.pop()
+    }
+  }
+}
