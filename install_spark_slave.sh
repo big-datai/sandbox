4efaed2489628273b8dbb38e17c6fb00c210be71
Dmitry <2dmitrypavlov@users.noreply.github.com>
2016-09-20 00:06:22 +0300

    Update install_spark_slave.sh

--- install_spark_slave.sh
+++ install_spark_slave.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
+#This script is for installation of spark slave on a linux - ubuntu
+
+echo "Utilities for linux:"
+
+sudo apt-get clean
+sudo apt-get update
+sudo apt-get install rpm
+#make sure linux is running jdk8 and all env are set...
+
+echo "Installing java"
+
+curl -v -j -k -L -H "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jdk-8u102-linux-x64.rpm > jdk-8u102-linux-x64.rpm
+rpm -Uvh jdk-8u102-linux-x64.rpm
+
 
