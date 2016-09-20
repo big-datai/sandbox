aaca53cbfc581892c92650ec8b2ade9d4949d6a4
Dmitry <2dmitrypavlov@users.noreply.github.com>
2016-09-20 11:47:37 +0300

    Update install_spark_slave.sh

--- install_spark_slave.sh
+++ install_spark_slave.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #This script is for installation of spark slave on a linux - ubuntu
+#It updates system install utils, jdk 8 and spark 2.0
 
 cd ~/
 echo "Utilities for linux:"
@@ -27,9 +28,49 @@ tar -zxvf spark-2.0.0-bin-hadoop2.7.tgz
 mv spark-2.0.0-bin-hadoop2.7 spark
 export SPARK_HOME=$(pwd)/spark
 export PATH=$SPARK_HOME/bin:$PATH
-
+#for example
+export MASTER_IP=192.168.179.160
 #Add system env to you shell 
 echo 'export SPARK_HOME=$(pwd)/spark'>>.bashrc
 echo 'export PATH=$SPARK_HOME/bin:$PATH'>>.bashrc
+
+#Here you need to allow your firewall all inner network connections
+#use ufw for iptables managment
+sudo iptables -F
+sudo ufw status numbered
+sudo ufw enable
+sudo ufw status numbered
+sudo ufw allow 22
+# allow access from you subnet, substitude this example ip with real
+# don't forget to allow local it on the master machine to allow it managing and accessing the slave
+
+# for example this one allows all subnetwork of kind 192.168.0.0 - 192.168.255.255
+sudo ufw allow from 192.168.0.0/16 to any
+
+# if you want to be more strict do for each specific ip tha you want to include in the network
+sudo ufw allow from "ip master" to any
+sudo ufw allow from "ip slave 1" to any
+
+sudo ufw allow 80
+sudo ufw allow 443
+
+#Allow connections to a specific network interface
+#sudo ufw allow in on eth1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
